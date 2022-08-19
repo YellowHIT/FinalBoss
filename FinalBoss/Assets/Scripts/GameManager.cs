fc5f713a1d553c5994e25c07be913ae08da1295a
YellowHIT <109045292+YellowHIT@users.noreply.github.com>
2022-08-19 18:06:53 -0300

    Background ready and BG Sky rotates

--- FinalBoss/Assets/Scripts/GameManager.cs
+++ FinalBoss/Assets/Scripts/GameManager.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject backgroundSky;
+
+    public float skyRotationSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RotateBackgroundSky();
+    }
+
+    public void RotateBackgroundSky()
+    {
+        backgroundSky.transform.Rotate(new Vector3(0, 0, 50) * skyRotationSpeed * Time.deltaTime);
+
     }
 }
