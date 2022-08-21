418e8c34fe84b922f565126673f69ae417d0eb7d
YellowHIT <109045292+YellowHIT@users.noreply.github.com>
2022-08-21 16:25:10 -0300

    StartMenu ready with art and script functions

---
+++ FinalBoss/Assets/Scripts/MenuStart.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuStart : MonoBehaviour
+{
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+
+        #if UNITY_EDITOR
+
+            EditorApplication.ExitPlaymode();
+    
+        #else
+        
+            Application.Quit();
+
+        #endif
+    }
+}
